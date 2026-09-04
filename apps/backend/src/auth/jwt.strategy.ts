10b4aa5b8c03e6b233212508a4640ff0aff45b4a
krisna1017 <kadekpermana05@gmail.com>
2026-09-04 10:07:09 +0800

    update jwt.strategy

--- apps/backend/src/auth/jwt.strategy.ts
+++ apps/backend/src/auth/jwt.strategy.ts
@@ -18,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         },
       ]),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: process.env.JWT_SECRET || 'secret',
     });
 
     console.log('=== JWT STRATEGY INITIALIZED ===');
